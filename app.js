29e429ff44278a05507244268af7dda4903efe84
safabeegum <pasafabeegum@gmail.com>
2024-08-28 17:16:42 +0530

    UserSignIn API Completed

--- app.js
+++ app.js
@@ -13,6 +13,44 @@ Mongoose.connect(
   "mongodb+srv://safabeegum:mongodb24@cluster0.pbzbbey.mongodb.net/waste_mngmt?retryWrites=true&w=majority&appName=Cluster0"
 );
 
+
+//UserSignIn API
+app.post("/signin", async(req,res)=>{
+    let input = req.body
+    let result = userModel.find({email:req.body.email}).then(
+            (items)=>{
+                if (items.length>0) 
+                {
+                    const passwordValidator = Bcrypt.compareSync(req.body.password, items[0].password)
+                    if (passwordValidator) 
+                    {
+                        jwt.sign({email:req.body.email},"waste_mngmt",{expiresIn:"1d"},
+                            (error,token)=>{
+                                if (error) 
+                                {
+                                    res.json({"status":"Error","Error":error})
+                                } 
+                                else 
+                                {
+                                    res.json({ status: "Success","token":token,"userId":items[0]._id });
+                                }
+                            })
+                    } 
+                    else 
+                    {
+                        res.json({"status":"Incorrect Password"})
+                    }
+                } 
+                else 
+                {
+                    res.json({"status":"Invalid Email ID"})
+                }
+            }
+    ).catch()
+})
+
+
+//UserSignUp API
 app.post("/signup", async (req, res) => {
   let input = req.body;
   let hashedPassword = Bcrypt.hashSync(req.body.password, 10);
